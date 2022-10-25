71bff3296a1dfa3b7aa8760ee8707cb59aee8cda
Павел Кузнецов <pavel_iv37@mail.ru>
2022-10-25 18:53:04 +0300

    corrected

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,19 +1,27 @@
 import brainGames from '../index.js';
 import random from '../utilit.js';
 
-const description = `Answer "yes" if given number is prime. Otherwise answer "no"`;
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+
+const isPrime = (number) => {
+  for (let i = number - 1; i > 1; i -= 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const prime = () => {
   let result = '';
   const question = random(1, 99);
-  for (let j = question - 1; j > 1; j -= 1) {
-    if (question % j !== 0) {
-      result = 'yes';
-    } else {
-      result = 'no';
-    }
+  if (isPrime(question) === true) {
+    result = 'yes';
+  } else {
+    result = 'no';
   }
-
   return [question, String(result)];
 };
+
 brainGames(description, prime);
 export default prime;
