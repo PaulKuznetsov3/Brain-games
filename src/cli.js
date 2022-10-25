a373787ca193d14f49028aace5e728aa6056fb09
Павел Кузнецов <pavel_iv37@mail.ru>
2022-10-25 14:38:09 +0300

    fixed hello

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 const nameDialog = () => {
   console.log('Welcome to the Brain Games!')
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hallo, ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 };
 export default nameDialog;
